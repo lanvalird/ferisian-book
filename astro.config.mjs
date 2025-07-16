aef9e7e6c0c2fc5978005a8a7fa8ff8dfa708ff6
lanvalird <strazhognay@gmail.com>
2025-07-16 12:42:59 +0400

    delete badges in sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,104 +1,109 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightBlog from 'starlight-blog'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightBlog from "starlight-blog";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Ferisian Book',
-    customCss: ["./src/styles/main.css"],
-    social: {
-      // github: 'https://github.com/aculaOne/ferisian_book',
-    },
-    defaultLocale: 'ru',
-    locales: {
-      ru: {
-        label: 'Русский',
-        lang: 'ru'
+  integrations: [
+    starlight({
+      title: "Ferisian Book",
+      customCss: ["./src/styles/main.css"],
+      social: {
+        // github: 'https://github.com/aculaOne/ferisian_book',
       },
-      fe: {
-        label: 'Пверiйснö',
-        lang: 'fe'
-      }
-    },
-    sidebar: [
-      {
-        label: 'Интро',
-        translations: {
-          'ru': "Интро",
-          'fe': "Старт"
+      defaultLocale: "ru",
+      locales: {
+        ru: {
+          label: "Русский",
+          lang: "ru",
         },
-        items: [
-          {
-            label: 'Интро',
-            link: 'intro',
-            translations: {
-              'ru': "Интро",
-              'fe': "Старт"
+        fe: {
+          label: "Пверiйснö",
+          lang: "fe",
+        },
+      },
+      sidebar: [
+        {
+          label: "Интро",
+          translations: {
+            ru: "Интро",
+            fe: "Старт",
+          },
+          items: [
+            {
+              label: "Интро",
+              link: "intro",
+              translations: {
+                ru: "Интро",
+                fe: "Старт",
+              },
             },
-          },
-        ]
-      },
-      {
-        label: 'Основы',
-        // autogenerate: {
-        //   directory: 'basics',
-        // },
-        translations: {
-          'ru': "Основы",
-          'fe': "Стайкы"
-        },
-        items: [{
-          label: 'Алфавит',
-          link: 'basics/alphabet',
-          translations: {
-            'ru': "Алфавит",
-            'fe': "Алпвавiт"
-          },
+          ],
         },
         {
-          label: 'Словарь',
-          link: 'basics/dictionary',
+          label: "Основы",
+          // autogenerate: {
+          //   directory: 'basics',
+          // },
           translations: {
-            'ru': "Словарь",
-            'fe': "Вордэлiст"
+            ru: "Основы",
+            fe: "Стайкы",
           },
-          badge: { text: "~", variant: 'caution' },
-        }, {
-          label: 'Словообразование',
-          link: 'basics/word_formation',
-          translations: {
-            'ru': "Словообразование",
-            'fe': "Вордэпвормiс"
-          },
+          items: [
+            {
+              label: "Алфавит",
+              link: "basics/alphabet",
+              translations: {
+                ru: "Алфавит",
+                fe: "Алпвавiт",
+              },
+            },
+            {
+              label: "Словарь",
+              link: "basics/dictionary",
+              translations: {
+                ru: "Словарь",
+                fe: "Вордэлiст",
+              },
+            },
+            {
+              label: "Словообразование",
+              link: "basics/word_formation",
+              translations: {
+                ru: "Словообразование",
+                fe: "Вордэпвормiс",
+              },
+            },
+            {
+              label: "Образование предложений",
+              link: "basics/sentence_formation",
+              translations: {
+                ru: "Образование предложений",
+                fe: "Сентесопвормiс",
+              },
+            },
+          ],
         },
         {
-          label: "Образование предложений",
-          link: "basics/sentence_formation",
-          translations: {
-            'ru': "Образование предложений",
-            'fe': "Сентесопвормiс"
+          label: "История",
+          autogenerate: {
+            directory: "history",
           },
-          badge: { text: "новое", variant: "success" }
+          translations: {
+            ru: "История",
+            fe: "Сказмiл",
+          },
+          badge: { text: "~", variant: "caution" },
         },
-        ]
-      },
-      {
-        label: 'История',
-        autogenerate: {
-          directory: 'history'
-        },
-        translations: {
-          'ru': "История",
-          'fe': "Сказмiл"
-        },
-        badge: { text: "(+)", variant: 'success' },
-      }
-    ],
+      ],
 
-    plugins: [starlightBlog({
-      title: 'Блог',
-    })],
-  }), react()]
+      plugins: [
+        starlightBlog({
+          title: "Блог",
+        }),
+      ],
+    }),
+    react(),
+  ],
 });
